707d8b4aeaafba22ca0c9daf7bc6e56377b2e5d9
Thiago.comba <thiagocomba@hotmail.com>
2025-07-01 15:08:37 -0300

    atualização

--- nerd_book_store/settings.py
+++ nerd_book_store/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -159,4 +159,4 @@ INTERNAL_IPS = [
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "thcmb.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "Thcmb.pythonanywhere.com"]
